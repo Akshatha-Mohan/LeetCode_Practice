e07f7f114f9c4e0d064acdfee24542695df49a33
Akshatha Mohan <52374337+Akshatha122@users.noreply.github.com>
2023-03-19 15:41:53 -0500

    Create Buy_sell_stock.py

---
+++ Buy_sell_stock.py
@@ -0,0 +1,43 @@
+"""
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+"""
+
+#BUY LOW SELL HIGH
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        LEFT_BOUND = 0
+        RIGHT_BOUND = 1
+        max_diff = 0
+        while(RIGHT_BOUND < len(prices)):
+            if(prices[LEFT_BOUND] < prices[RIGHT_BOUND]):
+                profit = prices[RIGHT_BOUND] - prices[LEFT_BOUND]
+                max_diff = max(max_diff, profit)
+            else:
+                LEFT_BOUND = RIGHT_BOUND
+            RIGHT_BOUND += 1
+        return max_diff
+      
+      
+      
+      
+      
+      
