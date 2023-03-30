ad5ffc5a158a868853284e6c9e5dcb449dbb765e
Akshatha Mohan <52374337+Akshatha122@users.noreply.github.com>
2023-03-30 09:30:06 -0500

    Create Binary_Tree_Level_order.py

---
+++ Trees/Binary_Tree_Level_order.py
@@ -0,0 +1,47 @@
+'''
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        q = collections.deque()
+        result = []
+        q.append(root)
+
+        while q:
+            qlen = len(q)
+            #sub list for every level
+            level = []            
+            for i in range(qlen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            #to make sure level is non empty
+            if level:
+                result.append(level)
+        return result
+      
+      
+      
+      
