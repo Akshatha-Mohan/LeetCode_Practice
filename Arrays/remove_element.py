4c0d38b0089e09f1060cfb9f764df16abbe8a597
Akshatha Mohan <52374337+Akshatha-Mohan@users.noreply.github.com>
2023-06-22 11:31:22 -0500

    Create remove_element.py

---
+++ Arrays/remove_element.py
@@ -0,0 +1,38 @@
+"""
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+IDEA: pointer traversing through array checking the elements, if val == nums[i] -> delete that element and move on, reduce n -= 1
+TC: O(N)
+SC: O(1)
+"""
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        n = len(nums)
+        i = 0
+        while i < n:
+            if nums[i] == val:
+                del nums[i]
+                n -= 1
+            else:
+                i += 1
+
+        return len(nums)
+
+
+
