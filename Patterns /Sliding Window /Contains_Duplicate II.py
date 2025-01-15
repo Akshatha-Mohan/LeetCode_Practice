0bd81c4a8cbef09bb5a8fbb1111e8b9177841e70
Akshatha Mohan <52374337+Akshatha-Mohan@users.noreply.github.com>
2025-01-15 14:13:52 -0500

    Update Contains_Duplicate II.py

--- Patterns /Sliding Window /Contains_Duplicate II.py
+++ Patterns /Sliding Window /Contains_Duplicate II.py
@@ -1,3 +1,12 @@
+"""
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] 
+and abs(i - j) <= k. 
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+"""
 class Solution(object):
     def containsNearbyDuplicate(self, nums, k):
         """
@@ -5,6 +14,7 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
+""" 1) HASHMAP """
         seen = {}
         
         for i, num in enumerate(nums):
@@ -14,3 +24,17 @@ class Solution(object):
             seen[num] = i
         
         return False
+
+""" 2) SLIDING WINDOW """
+        window = set()
+        left = 0
+        for right in range(len(nums)):
+            if right - left > k:
+                window.remove(nums[left])
+                left += 1
+            if nums[right] in window:
+                return True
+            window.add(nums[right])
+        return False
+
+        
